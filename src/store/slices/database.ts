0fb7848910e6ce84eb927b397eb9c622a7caa500
Yuri Nazarenko <m@rezident.org>
2026-08-20 08:50:30 +0300

    Add database status fetch action to Zustand store

--- src/store/slices/database.ts
+++ src/store/slices/database.ts
@@ -1,12 +1,28 @@
 import type { StateCreator } from 'zustand';
 
-import type { worker } from '~/worker/worker';
+import { worker } from '~/worker/worker';
 
 export interface DatabaseSlice {
     database: {
         status?: Awaited<ReturnType<typeof worker.database.getStatus>>;
+        operation?: 'CHECKING';
+
+        fetchStatus: VoidFunction;
     };
 }
-export const createDatabaseSlice: StateCreator<DatabaseSlice> = () => ({
-    database: {},
+
+const update = (
+    set: Parameters<StateCreator<DatabaseSlice>>[0],
+    data: Partial<DatabaseSlice['database']>,
+) => {
+    set(({ database }) => ({ database: { ...database, ...data } }));
+};
+
+export const createDatabaseSlice: StateCreator<DatabaseSlice> = (set) => ({
+    database: {
+        fetchStatus: async () => {
+            update(set, { operation: 'CHECKING' });
+            update(set, { operation: undefined, status: await worker.database.getStatus() });
+        },
+    },
 });
